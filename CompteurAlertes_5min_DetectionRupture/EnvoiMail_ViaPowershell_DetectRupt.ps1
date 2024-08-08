d1944a755fd5416937cac2317f175b0594b4e3ce
s9d9n0 <155744364+s9d9n0@users.noreply.github.com>
2024-08-08 15:17:35 +0200

    ajout powershell

---
+++ CompteurAlertes_5min_DetectionRupture/EnvoiMail_ViaPowershell_DetectRupt.ps1
@@ -0,0 +1,26 @@
+﻿
+$from = "xxx.xxx@xxx.fr"
+$to = "xxx.xxx@xxx.fr"
+
+$user = "idep@ad.xxx.intra"
+$secpasswd = ConvertTo-SecureString "xxx" -AsPlainText -Force
+
+#$user = "AD/PD0-xxx-SVC"
+#$secpasswd = ConvertTo-SecureString "DRWvPGUGTx+KwPR" -AsPlainText -Force
+#$secpasswd = ConvertTo-SecureString "WCs*5Zd@nlOrqz4" -AsPlainText -Force
+#$secpasswd = ConvertTo-SecureString '4RFVcde\"2ZSXwqa&' -AsPlainText -Force
+#$secpasswd = ConvertTo-SecureString '9SqV*PZ6cKYS#QHx0t#Q' -AsPlainText -Force
+#$secpasswd = ConvertTo-SecureString 'dzW^Wdq6O6LqeE+pcVrr' -AsPlainText -Force
+#$secpasswd = ConvertTo-SecureString '3ln6qD@haqwuGIiA*AW3' -AsPlainText -Force
+
+$mycreds = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
+
+$smtp = "smtp.appli.xxx.fr"
+$sujet = "Detection Rupture"
+
+$pj1 = "C:\Users\SIAR_ycg8l6\Docs\ProgrammesR\CompteurAlertes_5min_DetectionRupture\df_regroup.csv"
+$pj2 = "C:\Users\SIAR_ycg8l6\Docs\ProgrammesR\CompteurAlertes_5min_DetectionRupture\df_delta_group.csv"
+       
+$message = Get-Content -Path "C:\Users\SIAR_ycg8l6\Docs\ProgrammesR\CompteurAlertes_5min_DetectionRupture\message_DetectRupt.txt" -Raw
+
+Send-MailMessage -To $to -From $from -Subject $sujet -Attachments $pj1,$pj2 -Body $message -Credential $mycreds -port 587 -smtp $smtp -UseSsl
